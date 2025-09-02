516083fd5204e64409551392679f9d5d14b0c31a
Ala`a Al-Deen <oracledb53@gmail.com>
2025-09-02 22:05:41 +0300

    إضافة صفحات جديدة

---
+++ lib/subjectregister.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SubjectRegister extends StatelessWidget {
+  const SubjectRegister({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
